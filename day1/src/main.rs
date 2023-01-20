4b9159f6be72cc99818d85fcda2206ccdfcc79b9
Arthur Pigeon <pigeon.arthur@hotmail.fr>
2023-01-20 18:56:09 +0100

    day1 done

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,68 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::eyre::Context;
+use color_print::cprintln;
+use itertools::Itertools;
+
+#[allow(dead_code)]
+fn part1(input: &str) {
+    cprintln!("<bold>Part 1:</bold>");
+    let lines = input
+        .lines()
+        .map(|line| line.parse::<u32>().ok())
+        .collect::<Vec<_>>();
+    let max_lead = lines
+        .split(|line| line.is_none())
+        .map(|group| group.iter().map(|v| v.unwrap()).sum::<u32>())
+        .max();
+    cprintln!("Elven lead: <green>{:?}</green>", max_lead);
+}
+
+#[allow(dead_code)]
+fn part1_itertools(input: &str) {
+    cprintln!("<bold>Part 1:</bold>");
+    let max_lead = input
+        .lines()
+        .map(|line| line.parse::<u32>().ok())
+        .batching(|it| {
+            let mut sum = None;
+            while let Some(Some(v)) = it.next() {
+                sum = Some(sum.unwrap_or(0) + v);
+            }
+            sum
+        })
+        .max()
+        .unwrap_or_default();
+    cprintln!("Elven lead: <green>{}</green>", max_lead);
+}
+
+fn part2(input: &str) {
+    cprintln!("\n<bold>Part 2:</bold>");
+    let top3 = input
+        .lines()
+        .map(|line| line.parse::<u32>().ok())
+        .batching(|it| {
+            let mut sum = None;
+            while let Some(Some(v)) = it.next() {
+                sum = Some(sum.unwrap_or(0) + v);
+            }
+            sum
+        })
+        .sorted_by_key(|&v| std::cmp::Reverse(v))
+        .take(3)
+        .sum::<u32>();
+    cprintln!("Top 3 elven groups: <green>{}</green>", top3);
+}
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    let input = read_input()?;
+    part1_itertools(&input);
+    part2(&input);
+    Ok(())
+}
+
+fn read_input() -> color_eyre::Result<String> {
+    let filename = "input.txt";
+    let input = std::fs::read_to_string(filename).wrap_err(format!("Reading '{filename}'"))?;
+    Ok(input)
 }
